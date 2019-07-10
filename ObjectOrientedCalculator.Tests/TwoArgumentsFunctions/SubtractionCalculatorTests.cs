505e5b9863f662e6c1e7fd8a8e026c58156dfb0b
BashkirovaE <3.14e@mail.ru>
2019-07-10 14:47:58 +0600

    Added subtraction test

---
+++ ObjectOrientedCalculator.Tests/TwoArgumentsFunctions/SubtractionCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+using ObjectOrientedCalculator.TwoArgumentsFunctions;
+using NUnit.Framework;
+
+namespace ObjectOrientedCalculator.Tests.TwoArgumentsFunctions
+{
+    [TestFixture]
+    public class SubtractionCalculatorTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            double expected = -34;
+            double actual = new SubtractionCalculator().Calculate(-33, 1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
