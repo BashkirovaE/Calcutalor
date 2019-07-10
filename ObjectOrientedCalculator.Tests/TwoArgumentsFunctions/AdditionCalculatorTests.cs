8ea6b1607bbf433047e82196b978601fa76278f3
BashkirovaE <3.14e@mail.ru>
2019-07-10 14:37:57 +0600

    Added testing
    The new project was created, added test for addition function

---
+++ ObjectOrientedCalculator.Tests/TwoArgumentsFunctions/AdditionCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+using ObjectOrientedCalculator.TwoArgumentsFunctions;
+using NUnit.Framework;
+
+namespace ObjectOrientedCalculator.Tests.TwoArgumentsFunctions
+{
+    [TestFixture]
+    public class AdditionCalculatorTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            double expected = -32;
+            double actual = new AdditionCalculator().Calculate(-33, 1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
