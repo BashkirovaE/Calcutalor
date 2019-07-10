26fbd30b7a24a597b8825feeba73fb84e8651a24
BashkirovaE <3.14e@mail.ru>
2019-07-10 14:46:07 +0600

    Added multiply test

---
+++ ObjectOrientedCalculator.Tests/TwoArgumentsFunctions/MultiplyCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+using ObjectOrientedCalculator.TwoArgumentsFunctions;
+using NUnit.Framework;
+
+namespace ObjectOrientedCalculator.Tests.TwoArgumentsFunctions
+{
+    [TestFixture]
+    public class MultiplyCalculatorTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            double expected = 6;
+            double actual = new MultiplyCalculator().Calculate(2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
