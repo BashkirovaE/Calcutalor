bfa1714964cb6dab2fd35ff9e562caae6f7a6b6c
abc9702 <kononovpv@mail.ru>
2019-07-10 12:07:07 +0600

    added btns for new functions

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,18 +40,28 @@
             this.btn_div = new System.Windows.Forms.Button();
             this.btn_sin = new System.Windows.Forms.Button();
             this.btn_cos = new System.Windows.Forms.Button();
+            this.ln_x = new System.Windows.Forms.Button();
+            this.minus_x = new System.Windows.Forms.Button();
+            this.abs_x = new System.Windows.Forms.Button();
+            this.x_inv = new System.Windows.Forms.Button();
+            this.x_pow_2 = new System.Windows.Forms.Button();
+            this.rad_x = new System.Windows.Forms.Button();
+            this.deg_x = new System.Windows.Forms.Button();
+            this.min_x_y = new System.Windows.Forms.Button();
+            this.x_pow_y = new System.Windows.Forms.Button();
+            this.log_x_y = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtA
             // 
-            this.txtA.Location = new System.Drawing.Point(15, 27);
+            this.txtA.Location = new System.Drawing.Point(12, 27);
             this.txtA.Name = "txtA";
             this.txtA.Size = new System.Drawing.Size(100, 20);
             this.txtA.TabIndex = 0;
             // 
             // txtB
             // 
-            this.txtB.Location = new System.Drawing.Point(119, 27);
+            this.txtB.Location = new System.Drawing.Point(118, 27);
             this.txtB.Name = "txtB";
             this.txtB.Size = new System.Drawing.Size(100, 20);
             this.txtB.TabIndex = 1;
@@ -150,11 +160,111 @@
             this.btn_cos.UseVisualStyleBackColor = true;
             this.btn_cos.Click += new System.EventHandler(this.OneArgumentFunc_Click);
             // 
+            // ln_x
+            // 
+            this.ln_x.Location = new System.Drawing.Point(174, 140);
+            this.ln_x.Name = "ln_x";
+            this.ln_x.Size = new System.Drawing.Size(75, 23);
+            this.ln_x.TabIndex = 12;
+            this.ln_x.Text = "ln(x)";
+            this.ln_x.UseVisualStyleBackColor = true;
+            // 
+            // minus_x
+            // 
+            this.minus_x.Location = new System.Drawing.Point(255, 140);
+            this.minus_x.Name = "minus_x";
+            this.minus_x.Size = new System.Drawing.Size(75, 23);
+            this.minus_x.TabIndex = 13;
+            this.minus_x.Text = "-x";
+            this.minus_x.UseVisualStyleBackColor = true;
+            // 
+            // abs_x
+            // 
+            this.abs_x.Location = new System.Drawing.Point(255, 111);
+            this.abs_x.Name = "abs_x";
+            this.abs_x.Size = new System.Drawing.Size(75, 23);
+            this.abs_x.TabIndex = 14;
+            this.abs_x.Text = "abs(x)";
+            this.abs_x.UseVisualStyleBackColor = true;
+            // 
+            // x_inv
+            // 
+            this.x_inv.Location = new System.Drawing.Point(174, 111);
+            this.x_inv.Name = "x_inv";
+            this.x_inv.Size = new System.Drawing.Size(75, 23);
+            this.x_inv.TabIndex = 15;
+            this.x_inv.Text = "1/x";
+            this.x_inv.UseVisualStyleBackColor = true;
+            // 
+            // x_pow_2
+            // 
+            this.x_pow_2.Location = new System.Drawing.Point(12, 111);
+            this.x_pow_2.Name = "x_pow_2";
+            this.x_pow_2.Size = new System.Drawing.Size(75, 23);
+            this.x_pow_2.TabIndex = 16;
+            this.x_pow_2.Text = "x^2";
+            this.x_pow_2.UseVisualStyleBackColor = true;
+            // 
+            // rad_x
+            // 
+            this.rad_x.Location = new System.Drawing.Point(12, 140);
+            this.rad_x.Name = "rad_x";
+            this.rad_x.Size = new System.Drawing.Size(75, 23);
+            this.rad_x.TabIndex = 17;
+            this.rad_x.Text = "rad(x)";
+            this.rad_x.UseVisualStyleBackColor = true;
+            // 
+            // deg_x
+            // 
+            this.deg_x.Location = new System.Drawing.Point(93, 140);
+            this.deg_x.Name = "deg_x";
+            this.deg_x.Size = new System.Drawing.Size(75, 23);
+            this.deg_x.TabIndex = 18;
+            this.deg_x.Text = "deg(x)";
+            this.deg_x.UseVisualStyleBackColor = true;
+            // 
+            // min_x_y
+            // 
+            this.min_x_y.Location = new System.Drawing.Point(93, 111);
+            this.min_x_y.Name = "min_x_y";
+            this.min_x_y.Size = new System.Drawing.Size(75, 23);
+            this.min_x_y.TabIndex = 19;
+            this.min_x_y.Text = "min(x,y)";
+            this.min_x_y.UseVisualStyleBackColor = true;
+            // 
+            // x_pow_y
+            // 
+            this.x_pow_y.Location = new System.Drawing.Point(174, 82);
+            this.x_pow_y.Name = "x_pow_y";
+            this.x_pow_y.Size = new System.Drawing.Size(75, 23);
+            this.x_pow_y.TabIndex = 20;
+            this.x_pow_y.Text = "x^y";
+            this.x_pow_y.UseVisualStyleBackColor = true;
+            // 
+            // log_x_y
+            // 
+            this.log_x_y.Location = new System.Drawing.Point(255, 82);
+            this.log_x_y.Name = "log_x_y";
+            this.log_x_y.Size = new System.Drawing.Size(75, 23);
+            this.log_x_y.TabIndex = 21;
+            this.log_x_y.Text = "log x(y)";
+            this.log_x_y.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 121);
+            this.ClientSize = new System.Drawing.Size(340, 289);
+            this.Controls.Add(this.log_x_y);
+            this.Controls.Add(this.x_pow_y);
+            this.Controls.Add(this.min_x_y);
+            this.Controls.Add(this.deg_x);
+            this.Controls.Add(this.rad_x);
+            this.Controls.Add(this.x_pow_2);
+            this.Controls.Add(this.x_inv);
+            this.Controls.Add(this.abs_x);
+            this.Controls.Add(this.minus_x);
+            this.Controls.Add(this.ln_x);
             this.Controls.Add(this.btn_cos);
             this.Controls.Add(this.btn_sin);
             this.Controls.Add(this.btn_div);
@@ -188,6 +298,16 @@
         private System.Windows.Forms.Button btn_div;
         private System.Windows.Forms.Button btn_sin;
         private System.Windows.Forms.Button btn_cos;
+        private System.Windows.Forms.Button ln_x;
+        private System.Windows.Forms.Button minus_x;
+        private System.Windows.Forms.Button abs_x;
+        private System.Windows.Forms.Button x_inv;
+        private System.Windows.Forms.Button x_pow_2;
+        private System.Windows.Forms.Button rad_x;
+        private System.Windows.Forms.Button deg_x;
+        private System.Windows.Forms.Button min_x_y;
+        private System.Windows.Forms.Button x_pow_y;
+        private System.Windows.Forms.Button log_x_y;
     }
 }
 
