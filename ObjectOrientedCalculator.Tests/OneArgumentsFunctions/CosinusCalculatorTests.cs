5df1f1c530cea1a1920999e54153f3fb0c9156d9
BashkirovaE <3.14e@mail.ru>
2019-07-10 14:53:30 +0600

    added test for sin and cos functions

---
+++ ObjectOrientedCalculator.Tests/OneArgumentsFunctions/CosinusCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+using ObjectOrientedCalculator.OneArgumentsFunctions;
+using NUnit.Framework;
+
+namespace ObjectOrientedCalculator.Tests.OneArgumentsFunctions
+{
+    [TestFixture]
+    public class CosinusCalculatorTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            double expected = 1;
+            double actual = new CosinusCalculator().Calculate(0);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ObjectOrientedCalculator.Tests/OneArgumentsFunctions/SinusCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+using ObjectOrientedCalculator.OneArgumentsFunctions;
+using NUnit.Framework;
+
+namespace ObjectOrientedCalculator.Tests.OneArgumentsFunctions
+{
+    [TestFixture]
+    public class SinusCalculatorTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            double expected = 0;
+            double actual = new SinusCalculator().Calculate(0);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
